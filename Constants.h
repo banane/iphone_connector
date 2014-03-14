7c92691b92c4175fb10b07d1eaad56e1ac0ebbef
Anna Billstrom <banane@gmail.com>
2014-03-13 18:34:01 -0700

    Update Constants.h

--- Constants.h
+++ Constants.h
@@ -9,15 +9,15 @@
 #ifndef w2connect_Constants_h
 #define w2connect_Constants_h
 
-#define kAPIBaseURLString @"http://w2load-202588896.us-west-1.elb.amazonaws.com:3000"
+#define kAPIBaseURLString @"http://xxxx
 //#define kAPIBaseURLString @"http://localhost:3000"
 //#define kAPIBaseURLString @"http://10.48.164.39:3000"
 
 
 
-#define kAWSPublicAccessKey @"AKIAI6P3ZBS52HYSAJEA"
-#define kAWSSecretKey @"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"
+#define kAWSPublicAccessKey @"xxxx"
+#define kAWSSecretKey @"xxxx"
 
-#define testFlightToken @"75456710-2e74-46c0-a034-57293f61079e"
+#define testFlightToken @"xxxx"
 
 #endif
